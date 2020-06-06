3160256f878ab9628c28de66d31a21cbe33dc9ab
FelixL7 <felix.lipinski@t-online.de>
2020-06-06 17:47:14 +0200

    toaster wrapper min-height removed

--- resources/views/toaster.blade.php
+++ resources/views/toaster.blade.php
@@ -2,7 +2,7 @@
     $toasts = \FelixL7\Toast\Facades\Toaster::toasts()->merge(session('bootstrap-toasts'))
 @endphp
 @if ($toasts->isNotEmpty())
-    <div aria-live="polite" aria-atomic="true" style="position: absolute; top: 10px; right: 10px; width: 300px; min-height: 200px;">
+    <div aria-live="polite" aria-atomic="true" style="position: absolute; top: 10px; right: 10px; width: 300px;">
         {{--dd($toasts)--}}
         @foreach ($toasts as $toast)
             @includeFirst(['bootstrap-toast::toasts.'.$toast->type], ['toast' => $toast])
